331e6d64efb999e1aac8c04642fce151100d4c87
anvts <worontsov.andrey@gmail.com>
2024-01-21 23:01:46 +0300

    Implements possibility of using anonymous functions as route handler

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use Anvts\Framework\Routing\Route;
 use App\Controllers\HomeController;
 use App\Controllers\PostController;
+use Anvts\Framework\Http\Response;
 
 return [
     Route::get('/', [HomeController::class, 'index']),
     Route::get('/posts/{id:\d+}', [PostController::class, 'show']),
+    Route::get('/test/{str}', function (string $str) {
+        return new Response("Test $str");
+    }),
 ];
