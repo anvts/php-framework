4833b31bf88ecbd3eba10cb1b2af141d56765a2e
anvts <worontsov.andrey@gmail.com>
2024-01-21 15:03:34 +0300

    Add routes to Kernel class

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -2,11 +2,32 @@
 
 namespace Anvts\Framework\Http;
 
+use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher;
+
 class Kernel
 {
     public function handle(Request $request): Response
     {
-        $content = '<h1>Test</h1>';
-        return new Response($content);
+        $dispatcher = simpleDispatcher(function (RouteCollector $collector) {
+            $collector->get( '/', function () {
+                $content = '<h1>Root</h1>';
+                return new Response($content);
+            });
+
+            $collector->get( '/posts/{id:\d+}', function (array $vars) {
+                $content = "<h1>Post #{$vars['id']}</h1>";
+                return new Response($content);
+            });
+        });
+
+        $routeInfo = $dispatcher->dispatch(
+            $request->server['REQUEST_METHOD'],
+            $request->server['REQUEST_URI'],
+        );
+
+        [$status, $handler, $vars] = $routeInfo;
+
+        return $handler($vars);
     }
 }
