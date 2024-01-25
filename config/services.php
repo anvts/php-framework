59dd48190543e4dc04fe881c8ecff1cdef7a942d
anvts <worontsov.andrey@gmail.com>
2024-01-25 22:05:03 +0300

    Configure dotenv

--- config/services.php
+++ config/services.php
@@ -4,9 +4,13 @@ use League\Container\Container;
 use League\Container\Argument\Literal\ArrayArgument;
 use League\Container\Argument\Literal\StringArgument;
 use League\Container\ReflectionContainer;
+use Symfony\Component\Dotenv\Dotenv;
 use Anvts\Framework\Http\Kernel;
 use Anvts\Framework\Routing\RouterInterface;
 use Anvts\Framework\Routing\Router;
+
+$dotenv = new Dotenv();
+$dotenv->load(BASE_PATH . '/.env');
 
 // Application parameters
 
@@ -18,7 +22,9 @@ $container = new Container();
 
 $container->delegate(new ReflectionContainer(true));
 
-$container->add('APP_ENV', new StringArgument('local'));
+$appEnv = $_ENV['APP_ENV'] ?? 'local';
+
+$container->add('APP_ENV', new StringArgument($appEnv));
 
 $container->add(RouterInterface::class, Router::class);
 $container->extend(RouterInterface::class)
