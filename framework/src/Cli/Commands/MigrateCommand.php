ceb411fc93a917eeb3dd5c4afc20a8aefd4d565d
anvts <worontsov.andrey@gmail.com>
2024-01-27 04:08:25 +0300

    Create MigrateCommand class

---
+++ framework/src/Cli/Commands/MigrateCommand.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Anvts\Framework\Cli\Commands;
+
+use Anvts\Framework\Cli\CommandInterface;
+
+class MigrateCommand implements CommandInterface
+{
+    public function execute(array $args = []): int
+    {
+        return 0;
+    }
+}
