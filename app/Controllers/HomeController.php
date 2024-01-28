65025d46779561e43917d4e9f9a2668b4d047e48
anvts <worontsov.andrey@gmail.com>
2024-01-28 16:58:13 +0300

    Remove test service

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,20 +4,16 @@ namespace App\Controllers;
 
 use Anvts\Framework\Controller\AbstractController;
 use Anvts\Framework\Http\Response;
-use App\Services\TestService;
 
 class HomeController extends AbstractController
 {
     public function __construct(
-        private readonly TestService $testService,
     )
     {
     }
 
     public function index(): Response
     {
-        return $this->render('home.html.twig', [
-            'testUrl' => $this->testService->getTestUrl()
-        ]);
+        return $this->render('home.html.twig');
     }
 }
